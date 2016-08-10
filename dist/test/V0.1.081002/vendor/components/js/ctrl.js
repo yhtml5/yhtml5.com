fa22aa271262627290f2d78479475319f50a5cc7
张大漾丨前端开发 <1417376255@qq.com>
2016-08-10 13:55:27 +0800

    add design

---
+++ dist/test/V0.1.081002/vendor/components/js/ctrl.js
@@ -0,0 +1,12 @@
+/*
+ * @require components/js/router.js
+ * @require components/js/directive.js
+ */
+/************************* Ctrl *****************************/
+angular.module('controllers', [])
+    .controller('controllers', function($scope) {
+        S.init();
+    })
+    .controller('smsConfirmCtrl', function($scope) {
+
+    })
